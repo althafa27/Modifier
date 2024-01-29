570ea07e04b2b5357d1238b3443cac530c44dc08
althafa27 <150301631+althafa27@users.noreply.github.com>
2024-01-29 22:27:15 +0700

    Add files via upload

---
+++ Person_private.java
@@ -0,0 +1,13 @@
+package modifier;
+
+class Person_private {
+    private String name;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+}
---
+++ person2.java
@@ -0,0 +1,13 @@
+package modifier;
+
+public class person2 {
+    String name;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+}
